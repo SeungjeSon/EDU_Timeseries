858cb6663965034f135b989dd0742f33a410bbb0
SJ <seungje0927@gmail.com>
2023-01-10 22:10:20 +0900

    chapter3_1st Seasonality : 코드시작 ~ fourier 예측 전까지

---
+++ 3_Seasonality.py
@@ -0,0 +1,95 @@
+from pathlib import Path
+from warnings import simplefilter
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from statsmodels.tsa.deterministic import CalendarFourier, DeterministicProcess
+from scipy.signal import periodogram
+
+simplefilter("ignore")
+
+# Set Matplotlib defaults
+plt.style.use("seaborn-whitegrid")
+plt.rc("figure", autolayout=True, figsize=(11, 5))
+plt.rc(
+    "axes",
+    labelweight="bold",
+    labelsize="large",
+    titleweight="bold",
+    titlesize=16,
+    titlepad=10,
+)
+plot_params = dict(
+    color="0.75",
+    style=".-",
+    markeredgecolor="0.25",
+    markerfacecolor="0.25",
+    legend=False,
+)
+
+""" Function """
+# annotations: https://stackoverflow.com/a/49238256/5769929
+def seasonal_plot(X, y, period, freq, ax=None):
+    if ax is None:
+        _, ax = plt.subplots()
+    palette = sns.color_palette("husl", n_colors=X[period].nunique())
+    ax = sns.lineplot(x=freq, y=y, hue=period, data=X, ci=False, ax=ax, palette=palette, legend=False)
+    ax.set_title(f"Seaonal Plot ({period}/{freq})")
+    for line, name in zip(ax.lines, X[period].unique()):
+        y_ = line.get_ydata()[-1]
+        ax.annotate(name, xy=(1, y_), xytext=(6, 0), color=line.get_color(), xycoords=ax.get_yaxis_transform(), textcoords="offset points", size=14, va="center")
+    return ax
+
+
+def plot_periodogram(ts, detrend='linear', ax=None):
+    fs = pd.Timedelta("1Y") / pd.Timedelta("1D")
+    frequncies, spectrum = periodogram(ts, fs=fs, detrend=detrend, window="boxcar", scaling="spectrum")
+    if ax is None:
+        _, ax = plt.subplots()
+    ax.step(frequncies, spectrum, color="purple")
+    ax.set_xscale("log")
+    ax.set_xticks([1, 2, 4, 6, 12, 26, 52, 104])
+    ax.set_xticklabels(
+        [
+            "Annual (1)",
+            "Semiannual (2)",
+            "Quarterly (4)",
+            "Bimonthly (6)",
+            "Monthly (12)",
+            "Biweekly (26)",
+            "Weekly (52)",
+            "Semiweekly (104)"
+        ],
+        rotation=30
+    )
+    ax.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
+    ax.set_ylabel("Variance")
+    ax.set_title("Periodogram")
+    return ax
+
+
+""" 데이터 적용 """
+data_dir = Path("./input/ts-course-data")
+tunnel = pd.read_csv(data_dir / "tunnel.csv", parse_dates=["Day"])
+tunnel = tunnel.set_index("Day").to_period("D")
+
+X = tunnel.copy()
+# days within a week
+X["day"] = X.index.dayofweek
+X["week"] = X.index.week
+
+# days within a year
+X["dayofyear"] = X.index.dayofyear
+X["year"] = X.index.year
+fig, (ax0, ax1, ax2) = plt.subplots(3, 1, figsize=(11, 6))
+seasonal_plot(X, y="NumVehicles", period="week", freq="day", ax=ax0)
+seasonal_plot(X, y="NumVehicles", period="year", freq="dayofyear", ax=ax1)
+plot_periodogram(tunnel.NumVehicles, ax=ax2)
+# plt.show()
+
+fourier = CalendarFourier(freq="A", order=10)
+
+dp = DeterministicProcess(index=tunnel.index, constant=True, order=1, seasonal=True, additional_terms=[fourier], drop=True)
+
+X = dp.in_sample()
